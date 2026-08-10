9632cdb508a3d4f5748987ce8df0f6b63c19c3c9
AndraDEV2205 <tinystoresofc@gmail.com>
2026-08-10 16:28:53 -0300

    .

--- script.js
+++ script.js
@@ -3548,6 +3548,7 @@ const dicionarioTitulos = {
     
     'open': '<svg width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="#F1F5F9" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin-right: 8px;"><path d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 3.5z"></path></svg> Open Leaderboard'
 };
+
     
     // MUDANÇA REALIZADA AQUI DE innerText PARA innerHTML:
     const tituloEl = document.getElementById('titulo-cf-benchmark');
