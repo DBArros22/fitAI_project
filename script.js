94f884026aaee33cd7efd66de670b14fe1bd5bf9
AndraDev2205 <tinystoresofc@gmail.com>
2026-09-11 20:40:08 -0300

    ajuste em let = bancoDeDados

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ var db = window.db;
 
 // 2. VARIÁVEIS GLOBAIS DE ESTADO
 let usuarioAtualId = null;
-let bancoDeDados = JSON.parse(localStorage.getItem('fitai_pro_data')) || { fichas: {} };
+let bancoDeDados = JSON.parse(localStorage.getItem('fitai_pro_data')) || { fichas: {} }; 
 window.bancoDeDados = bancoDeDados;
 let diasTreinados = [];
 let lembretes = [];
